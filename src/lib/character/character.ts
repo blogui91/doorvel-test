9b3d10c5d481d3f7f562f2f4d378f1d2f81477ca
Cesar Santana <casc.santana@gmail.com>
2023-09-23 09:00:58 -0600

    fix(characters page): added empty state when results not found

--- src/lib/character/character.ts
+++ src/lib/character/character.ts
@@ -1,14 +1,27 @@
 import { api } from "@/http"
 import { Character } from "./types"
 import { ApiResponse } from "../types";
+import { AxiosError } from "axios";
+import { emptyResponse } from "@/utils";
+
 
 
 export const getCharacters = async (params = {}) => {
-  const response = await api.get<ApiResponse<Character>>('character', {
-    params,
-  });
+  try {
+    const response = await api.get<ApiResponse<Character>>('character', {
+      params,
+    });
+  
+    return response.data;
+  } catch (error: unknown) {
+    const err = error as AxiosError;
 
-  return response.data;
+    if (err.response?.status === 404) {
+      return emptyResponse()
+    }
+
+    throw error;
+  }
 }
 
 export const getCharacter = async (id: number|string) => {
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,15 @@
-export const randomString = () => `${Math.ceil(Math.random() * 100000)}-${Math.ceil(Math.random() * 100000)}`;
+export const randomString = () =>
+  `${Math.ceil(Math.random() * 100000)}-${Math.ceil(Math.random() * 100000)}`;
 
-export const arrayWithRandomValues = (itemsCount = 11) => 
-    new Array(itemsCount).fill(null).map(() => randomString());
+export const arrayWithRandomValues = (itemsCount = 11) =>
+  new Array(itemsCount).fill(null).map(() => randomString());
+
+export const emptyResponse = () => ({
+  info: {
+    count: 0,
+    pages: 0,
+    next: "",
+    prev: "",
+  },
+  results: [],
+});
